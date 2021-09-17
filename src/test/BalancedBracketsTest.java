9c0f3ff3d6fbccc4a1e4ba5e732d7034d25ba501
ShannonB320 <84356531+ShannonB320@users.noreply.github.com>
2021-09-16 20:15:00 -0500

    Completed Unit Testing studio

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,8 +1,8 @@
 package test;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
+import main.*;
 
 public class BalancedBracketsTest {
 
@@ -11,6 +11,62 @@ public class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void testForJustOpenAndCloseBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void testForEmptyString() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void testForStringBetweenBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void testForBracketsBetweenString() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void testForBracketsAdjacentToString() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    @Test
+    public void testForOpeningBracketOnly() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void testForBracketsFacingAway() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void testForBracketsFacingAwayInsideString() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    @Test
+    public void testForLonelyRightBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void testForLonelyLeftBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void testForExtraBracketWithoutPair() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch[Code]]"));
+    }
+
 
 
 }
